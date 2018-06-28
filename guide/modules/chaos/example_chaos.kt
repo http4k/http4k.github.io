0435ee7d76173011d8f1618677779987e59cc6d8
Travis CI User <travis@example.org>
2018-06-28 15:38:03 +0000

    Deployed 3a5fb80b with MkDocs version: 0.16.3

--- guide/modules/chaos/example_chaos.kt
+++ guide/modules/chaos/example_chaos.kt
@@ -4,6 +4,7 @@ import org.http4k.chaos.ChaosBehaviour
 import org.http4k.chaos.ChaosPolicy
 import org.http4k.chaos.ChaosStage.Companion.Wait
 import org.http4k.client.OkHttp
+import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
 import org.http4k.core.HttpTransaction
 import org.http4k.core.Method
@@ -23,19 +24,19 @@ fun main(args: Array<String>) {
 
     // chaos is split into "stages", which can be triggered by specific request or time-based criteria
     val doNothingStage = Wait.until { tx: HttpTransaction -> tx.request.method == POST }
-    val errorStage = ChaosPolicy.PercentageBased(100).inject(ChaosBehaviour.ReturnStatus(INTERNAL_SERVER_ERROR))
+    val errorStage = ChaosPolicy.PercentageBased(50).inject(ChaosBehaviour.ReturnStatus(INTERNAL_SERVER_ERROR))
 
     // chain the stages together with then() and finally convert to a standard http4k Filter
-    val chaosFilter = doNothingStage.then(errorStage).asFilter()
+    val chaosFilter: Filter = doNothingStage.then(errorStage).asFilter()
     val svc: HttpHandler = { Response(OK).body("A normal response") }
 
     chaosFilter.then(svc).asServer(SunHttp(9000)).start().use {
-        (1..5).forEach { performA(GET) }
+        (1..10).forEach { performA(GET) }
 
         // this triggers the change in behaviour
         performA(POST)
 
-        (1..5).forEach { performA(GET) }
+        (1..10).forEach { performA(GET) }
     }
 }
 
