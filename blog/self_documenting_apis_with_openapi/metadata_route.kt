be66c272538986096ad9b06f38b7b50bf819c479
Travis CI User <travis@example.org>
2019-06-30 13:16:48 +0000

    Deployed 8d2959cc with MkDocs version: 1.0.4

--- blog/self_documenting_apis_with_openapi/metadata_route.kt
+++ blog/self_documenting_apis_with_openapi/metadata_route.kt
@@ -1,4 +1,4 @@
-package blog.self_documenting_apis
+package blog.self_documenting_apis_with_openapi
 
 import org.http4k.contract.ContractRoute
 import org.http4k.contract.Tag
@@ -21,14 +21,14 @@ data class Drink(val name: String) {
 }
 
 object Greetings {
-    private val favouriteDrink = Query.map(::Drink).optional("drink", "your favourite beverage")
+    private val favouriteDrink = Query.map(::Drink).optional("drink", "Your favourite beverage")
 
     private fun handler(name: String, age: Age): HttpHandler = { req: Request ->
         val beverage = favouriteDrink(req)?.name ?: if (age.value >= 18) "beer" else "lemonade"
         Response(OK).body("Hello $name, would you like some $beverage?")
     }
 
-    operator fun invoke(): ContractRoute = "/greet" / Path.of("name") / Path.int().map(::Age).of("age") meta {
+    operator fun invoke(): ContractRoute = "/greet" / Path.of("name", "Your name") / Path.int().map(::Age).of("age", "Your age") meta {
         summary = "Send greetings"
         description = "Greets the stupid human by offering them a beverage suitable for their age"
         tags += Tag("query")
