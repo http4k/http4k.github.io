0b63650407ef0c23b2747528f33a86c9f5b82dbc
Travis CI User <travis@example.org>
2019-09-08 17:18:39 +0000

    Deployed 883efe9b with MkDocs version: 1.0.4

--- tutorials/self_documenting_apis_with_openapi/basic_contract.kt
+++ tutorials/self_documenting_apis_with_openapi/basic_contract.kt
@@ -1,4 +1,4 @@
-package blog.self_documenting_apis_with_openapi
+package tutorials.self_documenting_apis_with_openapi
 
 import org.http4k.contract.contract
 import org.http4k.contract.openapi.ApiInfo
@@ -6,13 +6,12 @@ import org.http4k.contract.openapi.v3.OpenApi3
 import org.http4k.contract.security.BasicAuthSecurity
 import org.http4k.core.Credentials
 import org.http4k.core.HttpHandler
-import org.http4k.format.Jackson
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
 
 fun main() {
     val http: HttpHandler = contract {
-        renderer = OpenApi3(ApiInfo("my empty but secure api", "v1.0", "Describing the purpose of the API"), Jackson)
+        renderer = OpenApi3(ApiInfo("my secure api", "v1.0", "API description"))
         descriptionPath = "/api/swagger.json"
         security = BasicAuthSecurity("realm", Credentials("user", "password"))
         routes += basicRoute
--- tutorials/self_documenting_apis_with_openapi/final_contract.kt
+++ tutorials/self_documenting_apis_with_openapi/final_contract.kt
@@ -1,4 +1,4 @@
-package blog.self_documenting_apis_with_openapi
+package tutorials.self_documenting_apis_with_openapi
 
 import org.http4k.contract.contract
 import org.http4k.contract.openapi.ApiInfo
@@ -8,10 +8,9 @@ import org.http4k.core.Credentials
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.format.Jackson
 
 val finalHttp: HttpHandler = contract {
-    renderer = OpenApi3(ApiInfo("my friendly api", "v1.0", "Describing the purpose of the API"), Jackson)
+    renderer = OpenApi3(ApiInfo("my friendly api", "v1.0", "API description"))
     descriptionPath = "/api/swagger.json"
     security = BasicAuthSecurity("realm", Credentials("user", "password"))
 
