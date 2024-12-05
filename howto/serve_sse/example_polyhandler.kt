d851bd4952dad4704f45efc73d11d9bf892179d2
daviddenton <daviddenton@users.noreply.github.com>
2024-12-05 14:37:11 +0000

    deploy: http4k/www@93db92a32ede380fb9a6f6d61bd9c48f3b1771ba

--- howto/serve_sse/example_polyhandler.kt
+++ howto/serve_sse/example_polyhandler.kt
@@ -1,7 +1,11 @@
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.ContentType
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.lens.Path
+import org.http4k.lens.accept
 import org.http4k.routing.sse
 import org.http4k.routing.sse.bind
 import org.http4k.server.PolyHandler
@@ -27,7 +31,7 @@ fun main() {
 
     val sse = sayHello.then(
         sse(
-            "/{name}" bind { req ->
+            "/hello/{name}" bind { req ->
                 SseResponse { sse: Sse ->
                     val name = namePath(req)
                     thread {
@@ -43,7 +47,26 @@ fun main() {
         )
     )
 
-    val http = { _: Request -> Response(OK).body("hiya world") }
+    val http = { req: Request -> Response(OK).body("hitting HTTP server: " + req.uri) }
 
     PolyHandler(http, sse = sse).asServer(Undertow(9000)).start()
+
+    val httpClient = JavaHttpClient()
+
+    // send an sse request - we need the event stream accept content type for it to be picked up by the Sse handler
+    httpClient(
+        Request(GET, "http://localhost:9000/hello/bob")
+            .accept(ContentType.TEXT_EVENT_STREAM)
+    ).bodyString().let(::println)
+
+    // if the SSE does not match it will fall back to HTTP
+    httpClient(
+        Request(GET, "http://localhost:9000/notbob")
+            .accept(ContentType.TEXT_EVENT_STREAM)
+    ).bodyString().let(::println)
+
+    // without the accept header, it will just be a normal http request
+    httpClient(
+        Request(GET, "http://localhost:9000/hello/no_accept_header")
+    ).bodyString().let(::println)
 }
