ac4ab1c469f929932d002a33105b05d3f2c3a1e9
daviddenton <daviddenton@users.noreply.github.com>
2025-03-28 14:05:23 +0000

    deploy: http4k/www@5bb2f13a258d89219724b33a329175c1ddfa177a

---
+++ ecosystem/http4k/reference/mcp/serverless_example.kt
@@ -0,0 +1,17 @@
+package content.ecosystem.http4k.reference.mcp
+
+import org.http4k.mcp.model.McpEntity
+import org.http4k.mcp.protocol.ServerMetaData
+import org.http4k.mcp.protocol.Version
+import org.http4k.routing.bind
+import org.http4k.routing.mcpHttpNonStreaming
+import org.http4k.serverless.ApiGatewayV2LambdaFunction
+import org.http4k.serverless.AppLoader
+
+// This function is an AWS Lambda function.
+class McpLambdaFunction : ApiGatewayV2LambdaFunction(AppLoader {
+    mcpHttpNonStreaming(
+        ServerMetaData(McpEntity.of("http4k mcp over serverless"), Version.of("0.1.0")),
+        toolDefinitionFor("David") bind diaryToolHandler
+    )
+})
