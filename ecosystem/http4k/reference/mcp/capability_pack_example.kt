b6e3fee4a523d06dbe5a13131f450ad89f9aeaa6
daviddenton <daviddenton@users.noreply.github.com>
2025-03-28 19:35:38 +0000

    deploy: http4k/www@ddd6c5ed76ede4671ea2d500c005cd3c5755937e

---
+++ ecosystem/http4k/reference/mcp/capability_pack_example.kt
@@ -0,0 +1,11 @@
+package content.ecosystem.http4k.reference.mcp
+
+import org.http4k.mcp.server.capability.CapabilityPack
+import org.http4k.routing.bind
+
+fun SetOfCapabilities() = CapabilityPack(
+    toolDefinitionFor("David") bind diaryToolHandler,
+    promptReference bind completionHandler,
+    websiteResource bind getLinksResourceHandler,
+    prompt bind greetingPromptHandler
+)
--- ecosystem/http4k/reference/mcp/simple_server_example.kt
+++ ecosystem/http4k/reference/mcp/simple_server_example.kt
@@ -4,6 +4,7 @@ import org.http4k.mcp.model.McpEntity
 import org.http4k.mcp.protocol.ProtocolCapability.ToolsChanged
 import org.http4k.mcp.protocol.ServerMetaData
 import org.http4k.mcp.protocol.Version
+import org.http4k.routing.bind
 import org.http4k.routing.mcpHttpStreaming
 import org.http4k.server.Helidon
 import org.http4k.server.asServer
@@ -13,6 +14,10 @@ fun main() {
     val mcpServer = mcpHttpStreaming(
         ServerMetaData(McpEntity.of("http4k MCP Server"), Version.of("1.0.0"), ToolsChanged),
         // bind server capabilities here ...
+        toolDefinitionFor("David") bind diaryToolHandler,
+        promptReference bind completionHandler,
+        websiteResource bind getLinksResourceHandler,
+        prompt bind greetingPromptHandler
     )
 
     // simply start it up!
