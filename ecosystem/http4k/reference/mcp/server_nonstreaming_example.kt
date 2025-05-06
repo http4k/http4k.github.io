56ce1b5f32c379ab8175e48ea54671c47ec16133
daviddenton <daviddenton@users.noreply.github.com>
2025-05-06 16:51:25 +0000

    deploy: http4k/www@7f221989903660073931faf62771ec7c9eda275c

---
+++ ecosystem/http4k/reference/mcp/server_nonstreaming_example.kt
@@ -0,0 +1,21 @@
+package content.ecosystem.http4k.reference.mcp
+
+import org.http4k.mcp.model.McpEntity
+import org.http4k.mcp.protocol.ServerMetaData
+import org.http4k.mcp.protocol.ServerProtocolCapability.ToolsChanged
+import org.http4k.mcp.protocol.Version
+import org.http4k.routing.bind
+import org.http4k.routing.mcpHttpNonStreaming
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+fun main() {
+    // this protocol version does not support SSE connections.
+    val mcpServer = mcpHttpNonStreaming(
+        ServerMetaData(McpEntity.of("http4k MCP Server"), Version.of("1.0.0"), ToolsChanged),
+        toolDefinitionFor("David") bind diaryToolHandler,
+    )
+
+    // simply start it up on any server you like!
+    mcpServer.asServer(SunHttp(3002)).start()
+}
