a4cd4d5b1149e2096ed21de87643d4853bfe9df9
daviddenton <daviddenton@users.noreply.github.com>
2025-05-09 16:24:50 +0000

    deploy: http4k/www@c30f673ec0a1b2515560ac7a522c9afade225099

---
+++ news/simplifying-mcp/server.kt
@@ -0,0 +1,25 @@
+package content.news.`simplifying-mcp`
+
+import org.http4k.core.Uri
+import org.http4k.mcp.ToolResponse
+import org.http4k.mcp.model.Content
+import org.http4k.mcp.model.McpEntity
+import org.http4k.mcp.model.Tool
+import org.http4k.mcp.protocol.ServerMetaData
+import org.http4k.mcp.protocol.Version
+import org.http4k.mcp.server.security.OAuthMcpSecurity
+import org.http4k.routing.bind
+import org.http4k.routing.mcpHttpStreaming
+import org.http4k.server.JettyLoom
+import org.http4k.server.asServer
+import java.time.Instant
+
+fun main() {
+    mcpHttpStreaming(
+        ServerMetaData(McpEntity.of("foo"), Version.of("bar")),
+        OAuthMcpSecurity(Uri.of("https://oauth-server")) { it == "my_oauth_token" },
+        Tool("time", "Get the current time") bind {
+            ToolResponse.Ok(listOf(Content.Text(Instant.now().toString())))
+        }
+    ).asServer(JettyLoom(3001)).start()
+}
